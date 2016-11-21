191f2e4672e212e567aa75e0677329651adbfea9
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-11-21 20:22:01 +0000

    initial check-in
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.containers/trunk@101122 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ setup.py
@@ -0,0 +1,71 @@
+import codecs
+from setuptools import setup, find_packages
+
+entry_points = {
+	'console_scripts': [
+		"nti_contentlibrary_indexer = nti.contentlibrary.scripts.constructor:main",
+	],
+}
+
+TESTS_REQUIRE = [
+	'fudge',
+	'nose2[coverage_plugin]',
+	'nti.testing',
+	'pyhamcrest',
+	'z3c.baseregistry',
+	'zope.testrunner',
+]
+
+def _read(fname):
+	with codecs.open(fname, encoding='utf-8') as f:
+		return f.read()
+
+setup(
+	name='nti.containers',
+	version=_read('version.txt').strip(),
+	author='Jason Madden',
+	author_email='jason@nextthought.com',
+	description="NTI containers",
+	long_description=_read('README.rst'),
+	license='Apache',
+	keywords='containers',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: Implementation :: CPython',
+		'Programming Language :: Python :: Implementation :: PyPy',
+	],
+	zip_safe=True,
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	include_package_data=True,
+	namespace_packages=['nti'],
+	tests_require=TESTS_REQUIRE,
+	install_requires=[
+		'setuptools',
+		'nti.dublincore'
+		'nti.ntiids',
+		'nti.zodb',
+		'repoze.lru',
+		'slugify',
+		'ZODB',
+		'zope.annotation',
+		'zope.component',
+		'zope.container',
+		'zope.dublincore',
+		'zope.interface',
+		'zope.location',
+		'zope.lifecycleevent',
+		'zope.site'
+	],
+	extras_require={
+		'test': TESTS_REQUIRE,
+	},
+	entry_points=entry_points,
+)
